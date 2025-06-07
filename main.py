66314083ed3d04ca637cdebee8638f662bcb2176
GamboaHMA <manuelgamboahernandez35@gmail.com>
2025-06-07 16:05:56 -0400

    api del modelo de lenguaje gemini pinchando

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import os
+from dotenv import load_dotenv
 from google import genai
 
-client = genai.Client(api_key="")
+load_dotenv()
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+client = genai.Client(api_key=google_api_key)
+
+response = client.models.generate_content(
+	model="gemini-2.0-flash",
+	contents="Explica como funciona la inteligencia artificial detalladamente y con ejemplos"
+)
+
+print(response.text)
+print("asd")
